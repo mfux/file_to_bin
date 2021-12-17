3ed9d63db9329c58d0b5aad9430bda38426fffed
Maximilian Fuchs <maxfuchs@protonmail.com>
2021-12-17 21:35:55 +0100

    WIP Flip Bits

--- file_to_bin.py
+++ file_to_bin.py
@@ -1,11 +1,29 @@
+import random
+
+def flip(bit):
+    if bit == "1":
+        return "0"
+    else:
+        return "1"
+
+def change(binary):
+    result = ''
+    for bit in binary:
+        if random.random() > 0.99:
+            result += flip(bit)
+        else:
+            result += bit
+
 binstrs = []
 print("processing")
-with open("/Users/mfuchs/Documents/file_to_bin/kanu.png", "rb") as f:
+with open("/Users/mfuchs/scm/file_to_bin/kanu.png", "rb") as f:
     ibytes = bytearray(f.read())
 for byte in ibytes:
     binary = bin(byte)[2:]
     while len(binary) < 8:
         binary = "0" + binary
+    print(type(binary))
+    # binary = change(binary)
     binstrs.append(binary)
 print(binstrs[:10])
 ibytes2 = bytearray([int(binstr, 2) for binstr in binstrs])
@@ -13,11 +31,11 @@ print(ibytes[:10])
 print(ibytes2[:10])
 
 # write binary data to file
-with open("/Users/mfuchs/Documents/file_to_bin/binarypic.txt", "w") as f:
+with open("/Users/mfuchs/scm/file_to_bin/binarypic.txt", "w") as f:
     f.write(''.join(binstrs))
 
 # write file copy from bytes
-with open("/Users/mfuchs/Documents/file_to_bin/copy.png", "wb") as f:
+with open("/Users/mfuchs/scm/file_to_bin/copy.png", "wb") as f:
     f.write(ibytes2)
     print("ok")
 
